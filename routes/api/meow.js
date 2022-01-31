b905d79064efec5d9f40e53b136be444605d70db
tharunoptimus <tharunoptimus@outlook.com>
2022-01-31 23:34:40 +0530

    Add post route for meow to get all meows

--- routes/api/meow.js
+++ routes/api/meow.js
@@ -11,4 +11,32 @@ router.get("/", (_, res) => {
 	res.send({ message: "meow meow" })
 })
 
+router.post("/all", async (_, res) => {
+	
+    try {
+        let latitute = parseFloat(req.body.latitute)
+        let longitude = parseFloat(req.body.longitude)
+
+        if (!latitute || !longitude) return res.status(400).send({ message: "Invalid coordinates" })
+
+        let meows = await Meow.find({
+            location: {
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [longitude, latitute]
+                    },
+                    $maxDistance: 1000
+                }
+            }
+        })
+
+        res.status(200).send(meows)
+
+	} catch (err) {
+		res.status(200).send({ message: "Unable to Query for some reason" })
+		console.log(err)
+	}
+})
+
 module.exports = router

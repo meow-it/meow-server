ce4ddfcacac748f6169cd080d987fc0ede6d3735
tharunoptimus <tharunoptimus@outlook.com>
2022-02-01 01:39:30 +0530

    Added route to add new meow

--- routes/api/meow.js
+++ routes/api/meow.js
@@ -40,6 +40,49 @@ router.post("/all", async (req, res) => {
 	}
 })
 
+router.post("/new", async (req, res) => {
+    try {
+
+        let userid = req.body.userid
+        if (!userid) return res.status(400).send({ message: "Invalid userid" })
+
+        let user = await User.findById(userid)
+        if (!user) return res.status(400).send({ message: "User Does not Exist" })
+
+        let text = req.body.text
+        if (!text) return res.status(400).send({ message: "Text Cannot be Null" })
+
+        let toxic = hasProfane(text)
+        let name = user.name
+        let profilePic = user.profilePic
+
+        let latitude = parseFloat(req.body.latitude)
+        let longitude = parseFloat(req.body.longitude)
+
+        if (!latitude || !longitude) return res.status(400).send({ message: "Invalid coordinates" })
+
+        let location = {
+            type: "Point",
+            coordinates: [longitude, latitude]
+        }
+
+        let meow = await Meow.create({
+            text,
+            toxic,
+            location,
+            name,
+            profilePic
+        })
+
+        res.status(201).send(meow)
+
+        
+    } catch (err) {
+        res.status(500).send({ message: "Unable to Create New Meow 😖" })
+        console.log(err)
+    }
+})
+
 function hasProfane(string) {
     let elements = string.split(" ")
     for (let i = 0; i < elements.length; i++) {

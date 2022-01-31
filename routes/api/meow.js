00c2cf8c2b607a81bb7c894bf038f5f32c2ce0ba
tharunoptimus <tharunoptimus@outlook.com>
2022-01-31 23:28:22 +0530

    Added meow.js route

---
+++ routes/api/meow.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const router = express.Router()
+const User = require("../../schemas/UserSchema")
+const Meow = require("../../schemas/MeowSchema")
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+router.get("/", (_, res) => {
+	res.send({ message: "meow meow" })
+})
+
+module.exports = router

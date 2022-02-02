2e105aae9da93a0b33129d7d4fd87679cd4e8867
tharunoptimus <tharunoptimus@outlook.com>
2022-02-02 15:41:37 +0530

    Extensive debugging for index creation

--- schemas/MeowSchema.js
+++ schemas/MeowSchema.js
@@ -1,19 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+let secondsToLive = 24 * 60 * 60
 const Schema = mongoose.Schema;
 const MeowSchema = new Schema({
     text: { type: String, required: true, trim: true },
     likes: { type: Number, default: 0 },
     toxic: { type: Boolean, default: false },
     location: { type: { type: String, default: 'Point' }, coordinates: [Number] },
-    createdAt: { type: Date, default: Date.now },
     name: { type: String, default: "" },
     profilePic: { type: String, default: "" },
     likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
-    isReviewed: { type: Boolean, default: false }
-    
+    isReviewed: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now, index: { expireAfterSeconds: secondsToLive } },
 });
 
-MeowSchema.index({ location: '2dsphere' }, { createdAt: 1 }, { expireAfterSeconds: 86400 });
+MeowSchema.index({ location: '2dsphere' });
 
 let Meow = mongoose.model('Meow', MeowSchema)
 module.exports = Meow;

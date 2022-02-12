598403bc911483331a23bb44afc3e573c507204a
tharunoptimus <tharunoptimus@outlook.com>
2022-02-12 20:32:25 +0530

    GET to get all comments for a given meowId

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -12,3 +12,19 @@ app.use(express.json())
 router.get("/", (_, res) => {
 	res.send({ message: "Comments API Online" })
 })
+
+router.get("/:id", async (req, res) => {
+	
+    try {
+
+        let meowId = req.params.id
+        let meow = await Meow.findById(meowId).populate({ path: "comments", options: { sort: { createdAt: -1 } } })
+        
+        if (!meow) return res.status(404).send({ message: "Meow with ID not found 🙁" })
+        
+        res.status(200).send(meow.comments)
+	} catch (err) {
+		res.status(200).send({ message: "Make sure you sent a correct meowid. Unable to Query for some reason 😦" })
+		console.log(err)
+	}
+})

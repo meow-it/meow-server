c6b87ed504aa1ed10cd9803cf38f82b3260bb301
tharunoptimus <tharunoptimus@outlook.com>
2022-02-12 20:32:38 +0530

    POST endpoint to create a new comment

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -28,3 +28,33 @@ router.get("/:id", async (req, res) => {
 		console.log(err)
 	}
 })
+
+router.post("/new", async (req, res) => {
+
+    try {
+
+        let { meowId, userId, text } = req.body
+        if(!meowId || !userId || !text) return res.status(400).send({ message: "Make sure you sent all the required fields 🙁: meowId, userId, text" })
+
+        let meow = await Meow.findById(meowId)
+        if (!meow) return res.status(404).send({ message: "Meow with ID not found 🙁" })
+
+        let user = await User.findById(userId)
+        if (!user) return res.status(404).send({ message: "User with ID not found 🙁" })
+
+        if (!text) return res.status(400).send({ message: "Comment Text cannot be null 😦" })
+
+        let toxic = hasProfane(text)
+        let name = user.name
+        let profilePic = user.profilePic
+
+        let comment = await Comment.create({ text, toxic, name, profilePic, commentedTo: meowId })
+        res.status(201).send(comment)
+
+        await Meow.findByIdAndUpdate(meowId, { $push: { comments: comment._id } })
+        
+    } catch (err) {
+        res.status(500).send({ message: "Unable to Create New Meow 😖" })
+        console.log(err)
+    }
+})

fa9f7349430572135c5398f9d1e93dc89ffaa6dd
tharunoptimus <tharunoptimus@outlook.com>
2022-02-12 20:33:50 +0530

    Rewritted the same function will refactor later

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -94,3 +94,26 @@ router.put("/review", async (req, res) => {
     }
 
 })
+
+function deepSearchForProfane(string) {
+
+    for (let i = 0; i < stuff.length; i++) {
+        if (string.toLowerCase().includes(stuff[i])) return true
+    }
+
+    return false
+}
+
+function hasProfane(string) {
+    let elements = string.split(" ")
+    for (let i = 0; i < elements.length; i++) {
+        let element = elements[i]
+        if (stuff.includes(element.toLowerCase())) {
+            return true
+        }
+    }
+
+    return false
+}
+
+module.exports = router

12ce92101e4e51fd3cb37374483afd70ca5fd127
tharunoptimus <tharunoptimus@outlook.com>
2022-02-19 15:22:23 +0530

    CORS middleware added

--- app.js
+++ app.js
@@ -3,21 +3,13 @@ dotenv.config()
 
 const express = require("express")
 const app = express()
-const cors = require("cors")
-const mongoose = require("./database")
+require("./database")
+const { cors } = require("./middleware")
 const port = process.env.PORT || 3003
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
 })
-
-app.use(
-	cors({
-		origin: "*",
-	})
-)
-
-app.options("*", cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -27,9 +19,9 @@ const registerAPI = require("./routes/api/register")
 const meowAPI = require("./routes/api/meow")
 const commentAPI = require("./routes/api/comment")
 
-app.use("/api/register", registerAPI)
-app.use("/api/meow", meowAPI)
-app.use("/api/comment", commentAPI)
+app.use("/api/register", cors, registerAPI)
+app.use("/api/meow", cors, meowAPI)
+app.use("/api/comment", cors, commentAPI)
 
 app.get("/", (_, res) => {
 	res.status(200).send({ text: "Hello World!"})

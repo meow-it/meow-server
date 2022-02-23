3a522594542bf32584913bcf1253affd1b3af73e
tharunoptimus <tharunoptimus@outlook.com>
2022-02-23 18:03:23 +0530

    Function to get content

--- routes/api/report.js
+++ routes/api/report.js
@@ -12,3 +12,20 @@ app.use(express.json())
 router.get("/", (_, res) => {
 	res.send({ message: "Report API Online" })
 })
+
+
+async function getContent(id, type) {
+    let content = null
+    try {
+        if(type == "meow") {
+            content = await Meow.findById(id)
+        } else if (type == "comment") {
+            content = await Comment.findById(id)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+
+    return content
+}
+

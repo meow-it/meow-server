3b827e67baa2739878504eddd346214ad7706724
tharunoptimus <tharunoptimus@outlook.com>
2022-02-23 18:04:47 +0530

    Added report `POST` endpoint

--- routes/api/report.js
+++ routes/api/report.js
@@ -13,6 +13,46 @@ router.get("/", (_, res) => {
 	res.send({ message: "Report API Online" })
 })
 
+router.post("/", async (req, res) => {
+
+    try {
+
+        let type = req.body.type
+        if (!type) return res.status(400).send({ message: "Invalid type" })
+
+        let id = req.body.id
+        if (!id) return res.status(400).send({ message: "Invalid id" })
+
+        let consernedId = req.body.consernedId
+        if (!consernedId) return res.status(400).send({ message: "Invalid consernedId" })
+        let conserned = await User.findById(consernedId)
+        if(conserned == null) return res.status(400).send({ message: "Conserned User Doesn't exist" })
+
+        let content = await getContent(id, type)
+        if (content == null) return res.status(404).send({ message: "Content with ID does not exist 🙁" })  
+        
+        if(content.flaggedBy.includes(conserned._id)) return res.status(202).send({ message: "Already flagged by you" })
+        
+        await incrementFlagCountToContent(id, type, consernedId)
+        
+        let userId = content.madeBy
+        let user = await User.findById(userId)
+        
+        if (user == null) return "User already got deleted"
+        
+        
+        if(content.flaggedBy.length + 1 > 2) {
+            await deleteContent(id, type)
+            await addFingerprintAndDeleteUser(userId, user)
+        }
+        
+        res.status(202).send({ message: "Content has been reported" })
+
+    } catch (err) {
+        res.status(500).send({ message: "Unable to Report 😖" })
+        console.log(err)
+    }
+})
 
 async function getContent(id, type) {
     let content = null

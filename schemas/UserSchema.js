eb3f632a3bb6fb438ee5e3ba4b2bde2629567791
tharunoptimus <tharunoptimus@outlook.com>
2022-02-23 15:23:16 +0530

    Added fields to user schema

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -4,7 +4,8 @@ const UserSchema = new Schema({
 	name: { type: String, required: true, trim: true },
     profilePic: { type: String },
     meows: [{ type: Schema.Types.ObjectId, ref: "Meow" }],
-    
+    isBanned: { type: Boolean, default: false },
+    fingerprint: { type: String, required: true }
 }, { timestamps: true });
 
 let User = mongoose.model('User', UserSchema)

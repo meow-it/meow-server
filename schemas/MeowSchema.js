c2d99cf7ea56774f1362200d9683942ec1547ec9
tharunoptimus <tharunoptimus@outlook.com>
2022-02-23 15:12:04 +0530

    Changed Meow schema to add createdBy field

--- schemas/MeowSchema.js
+++ schemas/MeowSchema.js
@@ -11,6 +11,7 @@ const MeowSchema = new Schema({
     likedBy: [{ type: Schema.Types.ObjectId, ref: "User" }],
     isReviewed: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now, index: { expireAfterSeconds: secondsToLive } },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User" },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }]
 });
 
